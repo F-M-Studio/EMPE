42c9fa93879212a4d786761a65b2a40935f48ee0
Mateusz Korniak <75249449+ZiQu-dev@users.noreply.github.com>
2025-06-27 23:46:24 +0200

    Poprawa jeszcze jednego błędnego tłumaczenia

--- translations/lidar_en.ts
+++ translations/lidar_en.ts
@@ -28,4 +28,447 @@
 -->
 <!DOCTYPE TS>
 <TS version="2.1" language="en_US">
+    <context>
+        <name>AboutUsDialog</name>
+        <message>
+            <source>About Us</source>
+            <translation>About Us</translation>
+        </message>
+        <message>
+            <source>EMPE Team</source>
+            <translation>EMPE Team</translation>
+        </message>
+    </context>
+    <context>
+        <name>AppMenu</name>
+        <message>
+            <source>File</source>
+            <translation>File</translation>
+        </message>
+        <message>
+            <source>Show Graph</source>
+            <translation>Show Graph</translation>
+        </message>
+        <message>
+            <source>Port Settings</source>
+            <translation>Port Settings</translation>
+        </message>
+        <message>
+            <source>Exit</source>
+            <translation>Exit</translation>
+        </message>
+        <message>
+            <source>Language</source>
+            <translation>Language</translation>
+        </message>
+        <message>
+            <source>English</source>
+            <translation>English</translation>
+        </message>
+        <message>
+            <source>Polish</source>
+            <translation>Polish</translation>
+        </message>
+        <message>
+            <source>Help</source>
+            <translation>Help</translation>
+        </message>
+        <message>
+            <source>About Us</source>
+            <translation>About Us</translation>
+        </message>
+    </context>
+    <context>
+        <name>GraphWindow</name>
+        <message>
+            <source>Graph</source>
+            <translation>Graph</translation>
+        </message>
+        <message>
+            <source>Stop</source>
+            <translation>Stop</translation>
+        </message>
+        <message>
+            <source>Start</source>
+            <translation>Start</translation>
+        </message>
+        <message>
+            <source>Clear Graph</source>
+            <translation>Clear Graph</translation>
+        </message>
+        <message>
+            <source>Time (min:ms)</source>
+            <translation>Time (min:ms)</translation>
+        </message>
+        <message>
+            <source>Distance</source>
+            <translation>Distance</translation>
+        </message>
+        <message>
+            <source>Recording period [ms]:</source>
+            <translation>Recording period [ms]:</translation>
+        </message>
+        <message>
+            <source>Set Y</source>
+            <translation>Set Y</translation>
+        </message>
+        <message>
+            <source>Y axis range:</source>
+            <translation>Y axis range:</translation>
+        </message>
+        <message>
+            <source>Auto-scroll X axis</source>
+            <translation>Auto-scroll X axis</translation>
+        </message>
+        <message>
+            <source>Points limit:</source>
+            <translation>Points limit:</translation>
+        </message>
+        <message>
+            <source>Smooth Graph</source>
+            <translation>Smooth Graph</translation>
+        </message>
+        <message>
+            <source>Smoothing:</source>
+            <translation>Smoothing:</translation>
+        </message>
+        <message>
+            <source>Use Relative Time</source>
+            <translation>Use Relative Time</translation>
+        </message>
+        <message>
+            <source>Show Series 1</source>
+            <translation>Show Series 1</translation>
+        </message>
+        <message>
+            <source>Show Series 2</source>
+            <translation>Show Series 2</translation>
+        </message>
+    </context>
+    <context>
+        <name>MainWindow</name>
+        <message>
+            <source>EMPE</source>
+            <translation>EMPE</translation>
+        </message>
+        <message>
+            <source>This program was created as part of the Embodying Math&amp;Physics Education project 2023-1-PL01-KA210-SCH-000165829</source>
+            <translation>This program was created as part of the Embodying Math&amp;Physics Education project 2023-1-PL01-KA210-SCH-000165829</translation>
+        </message>
+        <message>
+            <source>Drop Sensitivity (mm):</source>
+            <translation>Drop Sensitivity (mm):</translation>
+        </message>
+        <message>
+            <source>Reset All</source>
+            <translation>Reset All</translation>
+        </message>
+        <message>
+            <source>Sensor 1</source>
+            <translation>Sensor 1</translation>
+        </message>
+        <message>
+            <source>Enable Sensor 1</source>
+            <translation>Enable Sensor 1</translation>
+        </message>
+        <message>
+            <source>Drops: 0</source>
+            <translation>Drops: 0</translation>
+        </message>
+        <message>
+            <source>Stoper: 00:00.000</source>
+            <translation>Stopwatch: 00:00.000</translation>
+        </message>
+        <message>
+            <source>Sensor 2</source>
+            <translation>Sensor 2</translation>
+        </message>
+        <message>
+            <source>Enable Sensor 2</source>
+            <translation>Enable Sensor 2</translation>
+        </message>
+        <message>
+            <source>Stop Stoper</source>
+            <translation>Stop Stopwatch</translation>
+        </message>
+        <message>
+            <source>Start Stoper</source>
+            <translation>Start Stopwatch</translation>
+        </message>
+        <message>
+            <source>Stop Stoper (%1)</source>
+            <translation>Stop Stopwatch (%1)</translation>
+        </message>
+        <message>
+            <source>Stoper</source>
+            <translation>Stopwatch</translation>
+        </message>
+        <message>
+            <source>Time's up!</source>
+            <translation>Time's up!</translation>
+        </message>
+        <message>
+            <source>Reset Complete</source>
+            <translation>Reset Complete</translation>
+        </message>
+        <message>
+            <source>Drop counters, logs, and stoper have been reset.</source>
+            <translation>Drop counters, logs, and stopwatch have been reset.</translation>
+        </message>
+        <message>
+            <source>No Data</source>
+            <translation>No Data</translation>
+        </message>
+        <message>
+            <source>No drop events to save.</source>
+            <translation>No drop events to save.</translation>
+        </message>
+        <message>
+            <source>Save Drop Logs</source>
+            <translation>Save Drop Logs</translation>
+        </message>
+        <message>
+            <source>CSV Files (*.csv)</source>
+            <translation>CSV Files (*.csv)</translation>
+        </message>
+        <message>
+            <source>Error</source>
+            <translation>Error</translation>
+        </message>
+        <message>
+            <source>Cannot write file %1:
+                %2.</source>
+            <translation>Cannot write file %1:
+                %2.</translation>
+        </message>
+        <message>
+            <source>Success</source>
+            <translation>Success</translation>
+        </message>
+        <message>
+            <source>Drop logs saved to %1
+                Total events: %2</source>
+            <translation>Drop logs saved to %1
+                Total events: %2</translation>
+        </message>
+        <message>
+            <source>[%1] Automatic start of stopwatch 1 after drop detection</source>
+            <translation>[%1] Automatic start of stopwatch 1 after drop detection</translation>
+        </message>
+        <message>
+            <source>[%1] Automatic stop of stopwatch 1 after drop detection</source>
+            <translation>[%1] Automatic stop of stopwatch 1 after drop detection</translation>
+        </message>
+        <message>
+            <source>[%1] Automatic start of stopwatch 2 after drop detection</source>
+            <translation>[%1] Automatic start of stopwatch 2 after drop detection</translation>
+        </message>
+        <message>
+            <source>[%1] Automatic stop of stopwatch 2 after drop detection</source>
+            <translation>[%1] Automatic stop of stopwatch 2 after drop detection</translation>
+        </message>
+        <message>
+            <source>Drops: %1</source>
+            <translation>Drops: %1</translation>
+        </message>
+        <message>
+            <source>Start</source>
+            <translation>Start</translation>
+        </message>
+        <message>
+            <source>Stop</source>
+            <translation>Stop</translation>
+        </message>
+        <message>
+            <source>PORT settings</source>
+            <translation>PORT settings</translation>
+        </message>
+        <message>
+            <source>Show GRAPH</source>
+            <translation>Show GRAPH</translation>
+        </message>
+        <message>
+            <source>SAVE data 1</source>
+            <translation>SAVE data 1</translation>
+        </message>
+        <message>
+            <source>SAVE data 2</source>
+            <translation>SAVE data 2</translation>
+        </message>
+        <message>
+            <source>Clear GRAPH</source>
+            <translation>Clear GRAPH</translation>
+        </message>
+        <message>
+            <source>Show raw data</source>
+            <translation>Show raw data</translation>
+        </message>
+        <message>
+            <source>Hide raw data</source>
+            <translation>Hide raw data</translation>
+        </message>
+        <message>
+            <source>Distance:</source>
+            <translation>Distance:</translation>
+        </message>
+        <message>
+            <source>Global Time</source>
+            <translation>Global Time</translation>
+        </message>
+        <message>
+            <source>Always on Top</source>
+            <translation>Always on Top</translation>
+        </message>
+        <message>
+            <source>Save Data</source>
+            <translation>Save Data</translation>
+        </message>
+        <message>
+            <source>File exists</source>
+            <translation>File exists</translation>
+        </message>
+        <message>
+            <source>The file %1 already exists.
+                Do you want to replace it?</source>
+            <translation>The file %1 already exists.
+                Do you want to replace it?</translation>
+        </message>
+        <message>
+            <source>Distance,Time (mm:ss),Milliseconds,Raw Time (ms)
+            </source>
+            <translation>Distance,Time (mm:ss),Milliseconds,Raw Time (ms)
+            </translation>
+        </message>
+        <message>
+            <source>Data has been saved to %1</source>
+            <translation>Data has been saved to %1</translation>
+        </message>
+        <message>
+            <source>Stoper: %1:%2.%3</source>
+            <translation>Stopwatch: %1:%2.%3</translation>
+        </message>
+        <message>
+            <source>Failed to open port %1: %2</source>
+            <translation>Failed to open port %1: %2</translation>
+        </message>
+        <message>
+            <source>Stopped reading from port 1</source>
+            <translation>Stopped reading from port 1</translation>
+        </message>
+        <message>
+            <source>Stopped reading from port 2</source>
+            <translation>Stopped reading from port 2</translation>
+        </message>
+        <message>
+            <source>[%1] Drop detected (sensor %2): Previous distance: %3, Current: %4, Difference: %5</source>
+            <translation>[%1] Drop detected (sensor %2): Previous distance: %3, Current: %4, Difference: %5</translation>
+        </message>
+    </context>
+    <context>
+        <name>PortSettings</name>
+        <message>
+            <source>Port Settings</source>
+            <translation>Port Settings</translation>
+        </message>
+        <message>
+            <source>Sensor 1 Settings</source>
+            <translation>Sensor 1 Settings</translation>
+        </message>
+        <message>
+            <source>Port:</source>
+            <translation>Port:</translation>
+        </message>
+        <message>
+            <source>Baud Rate:</source>
+            <translation>Baud Rate:</translation>
+        </message>
+        <message>
+            <source>Data Bits:</source>
+            <translation>Data Bits:</translation>
+        </message>
+        <message>
+            <source>Stop Bits:</source>
+            <translation>Stop Bits:</translation>
+        </message>
+        <message>
+            <source>Parity:</source>
+            <translation>Parity:</translation>
+        </message>
+        <message>
+            <source>Flow Control:</source>
+            <translation>Flow Control:</translation>
+        </message>
+        <message>
+            <source>None</source>
+            <translation>None</translation>
+        </message>
+        <message>
+            <source>Even</source>
+            <translation>Even</translation>
+        </message>
+        <message>
+            <source>Odd</source>
+            <translation>Odd</translation>
+        </message>
+        <message>
+            <source>Space</source>
+            <translation>Space</translation>
+        </message>
+        <message>
+            <source>Mark</source>
+            <translation>Mark</translation>
+        </message>
+        <message>
+            <source>Hardware</source>
+            <translation>Hardware</translation>
+        </message>
+        <message>
+            <source>Software</source>
+            <translation>Software</translation>
+        </message>
+        <message>
+            <source>1</source>
+            <translation>1</translation>
+        </message>
+        <message>
+            <source>1.5</source>
+            <translation>1.5</translation>
+        </message>
+        <message>
+            <source>2</source>
+            <translation>2</translation>
+        </message>
+        <message>
+            <source>Sensor 2 Settings</source>
+            <translation>Sensor 2 Settings</translation>
+        </message>
+        <message>
+            <source>Apply</source>
+            <translation>Apply</translation>
+        </message>
+        <message>
+            <source>Close</source>
+            <translation>Close</translation>
+        </message>
+        <message>
+            <source>Refresh</source>
+            <translation>Refresh</translation>
+        </message>
+        <message>
+            <source>Error</source>
+            <translation>Error</translation>
+        </message>
+        <message>
+            <source>Please select different ports for Sensor 1 and Sensor 2.</source>
+            <translation>Please select different ports for Sensor 1 and Sensor 2.</translation>
+        </message>
+        <message>
+            <source>Settings Saved</source>
+            <translation>Settings Saved</translation>
+        </message>
+        <message>
+            <source>Port settings have been saved.</source>
+            <translation>Port settings have been saved.</translation>
+        </message>
+    </context>
 </TS>
